19443aa101548606dc435a399099b666bb62e6f9
Ali-J5 <alijebril5s.b@gmail.com>
2026-08-01 19:54:54 -0500

    feat: scaffold Spring Boot 4.1 project with Web and Actuator dependencies

---
+++ src/main/java/com/pricetracker/api/PriceTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pricetracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceTrackerApiApplication.class, args);
+	}
+
+}
